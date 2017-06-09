67e9086c3c0315f873d9efed7a17ac61b46818ae
Rachel Berkowitz <brkwtzr@gmail.com>
2017-06-09 19:18:34 -0400

    submit button in nav

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -14,13 +14,19 @@ export default class Navigation extends React.Component {
   render() {
     const categories = this.props.allCategories
     const user = this.props.user
+    const loggedOutFeats = (
+      <div>
+        <Login/>
+        <Link to={`/signup`}><button className="btn btn-default" type="button">Sign Up</button></Link>
+      </div>
+      )
     return (
       <Navbar>
         <Navbar.Header>
           <Navbar.Brand>{'\u2728'} The Bling Ring</Navbar.Brand>
         </Navbar.Header>
         <Nav>
-          {user ? <WhoAmI/> : <Login/>}
+          {user ? <WhoAmI/> : loggedOutFeats}
           {categories.map(category => <LinkContainer to={`/products/categories/${category.id}`} key={category.id}><NavItem>{category.name}</NavItem></LinkContainer>)}
         </Nav>
       </Navbar>
