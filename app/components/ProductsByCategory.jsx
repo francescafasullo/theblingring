10d1ed8395428b9ecc3855f69ac2c0da7ce2374f
Betty Lin <bettyhappylin@gmail.com>
2017-06-09 15:47:07 -0400

    pass in props

--- app/components/ProductsByCategory.jsx
+++ app/components/ProductsByCategory.jsx
@@ -2,17 +2,21 @@ import React from 'react'
 import {Grid, Col, Thumbnail} from 'react-bootstrap'
 
 export default(props) => {
+  const products = this.props.allProducts
+
   return (
     <div>
       <Grid>
         {
-          /*
-          map through all products of given category
-          <Col xs={6} md={4} key={product.id}>
-            <Thumbnail src={product.photos[0]}>
-            <h5>{product.title}</h5>
-          </Col>
-          */
+          products.map(product => {
+            return (
+            <Col xs={6} md={4} key={product.id}>
+              <Thumbnail src={product.photos[0]}>
+              <h5>{product.title}</h5>
+              </Thumbnail>
+            </Col>
+            )
+          })
         }
       </Grid>
     </div>
