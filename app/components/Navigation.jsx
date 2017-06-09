d7c31e878015f7837b41598db7c747cd99fb405d
Rachel Berkowitz <brkwtzr@gmail.com>
2017-06-09 16:28:12 -0400

    login and whoami in navbar

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Navbar, Nav, NavItem } from 'react-bootstrap'
 import {Link} from 'react-router'
+import WhoAmI from './WhoAmI'
+import Login from './Login'
+import SignUp from './SignUp'
 
 export default class Navigation extends React.Component {
   componentDidMount() {
@@ -9,19 +12,15 @@ export default class Navigation extends React.Component {
 
   render() {
     const categories = this.props.allCategories
+    const user = this.props.user
     return (
       <Navbar>
         <Navbar.Header>
           <Navbar.Brand>{'\u2728'} The Bling Ring</Navbar.Brand>
         </Navbar.Header>
         <Nav>
-          {
-            categories.map(category => <NavItem key={category.id}><Link to={`/products/categories/${category.id}`}>{category.name}</Link></NavItem>)
-
-            /* {user ? <WhoAmI/> : <Login/>}
-            cart stuff here too
-             */
-          }
+          {user ? <WhoAmI/> : <Login/>}
+          {categories.map(category => <NavItem key={category.id}><Link to={`/products/categories/${category.id}`}>{category.name}</Link></NavItem>)}
         </Nav>
       </Navbar>
     )
