8a82dc2e6d487fc2a3d01d083a647bbd37feea93
Betty Lin <bettyhappylin@gmail.com>
2017-06-11 15:09:39 -0400

    create homepage component, add style and images, replace Jokes component with new Homepage component

--- app/main.jsx
+++ app/main.jsx
@@ -6,6 +6,7 @@ import {connect, Provider} from 'react-redux'
 
 import store from './store'
 import Jokes from './components/Jokes'
+import Homepage from './components/Homepage'
 import NotFound from './components/NotFound'
 import SignUp from './components/SignUp'
 import ProductDetailsContainer from './containers/ProductDetailsContainer'
@@ -41,9 +42,9 @@ render(
   <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={RootApp}>
-        <IndexRedirect to="/jokes" />
+        <IndexRedirect to="/home" />
         <Route path ="/signup" component={SignUp} />
-        <Route path="/jokes" component={Jokes} />
+        <Route path="/home" component={Homepage} />
         <Route path="/products/:productId" component={ProductDetailsContainer} onEnter={onSingleProductEnter}/>
         <Route path="/products/categories/:categoryId" component={ProductsByCategoryContainer} onEnter={onSingleCategoryEnter} />
       </Route>
