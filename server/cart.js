94521ac8e8da124f5d2976006d6578c0e21e0cab
Francesca Fasullo <fjf2107@columbia.edu>
2017-06-12 12:05:12 -0400

    backend route for adding products to carts

--- server/cart.js
+++ server/cart.js
@@ -1,15 +1,34 @@
-// const app = require('APP'), {env} = app
-// const debug = require('debug')(`${app.name}:cart`)
+const app = require('APP'), {env} = app
+const debug = require('debug')(`${app.name}:cart`)
 
-// const {Cart, OAuth} = require('APP/db')
-// const cart = require('express').Router()
+const {Cart, CartProduct} = require('APP/db')
+const cart = require('express').Router()
 
-// cart.post('/cart/:userId', (req, res, next) => {
-//   Cart.findOne({
-//     where: {
-//       user_id: req.params.userId
-//     }
-//   })
-//   .then(cart => res.cart)
-//   .catch(next)
-// })
+cart.get('/:userId', (req, res, next) => {
+  Cart.findOne({
+    where: {
+      user_id: req.params.userId
+    }
+  })
+  .then(cart => res.send(cart))
+  .catch(next)
+})
+
+cart.post('/:userId/:productId', (req, res, next) => {
+  Cart.findOne({
+    where: {
+      user_id: req.params.userId
+    }
+  })
+  .then(cart => {
+    return CartProduct.create({
+      cart_id: cart.id,
+      product_id: req.params.productId,
+      quantity: 1
+    })
+  })
+  .then(item => res.send(item))
+  .catch(next)
+})
+
+module.exports = cart
