cb56f43be497e4e653f27dbf9d797f1531b30ec5
Francesca Fasullo <fjf2107@columbia.edu>
2017-06-12 11:12:46 -0400

    added cart backend file

---
+++ server/cart.js
@@ -0,0 +1,15 @@
+// const app = require('APP'), {env} = app
+// const debug = require('debug')(`${app.name}:cart`)
+
+// const {Cart, OAuth} = require('APP/db')
+// const cart = require('express').Router()
+
+// cart.post('/cart/:userId', (req, res, next) => {
+//   Cart.findOne({
+//     where: {
+//       user_id: req.params.userId
+//     }
+//   })
+//   .then(cart => res.cart)
+//   .catch(next)
+// })
