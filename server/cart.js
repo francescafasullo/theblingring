340ed268b40b76d7dc1829727e4e661e637d4b43
Rachel Berkowitz <brkwtzr@gmail.com>
2017-06-12 16:47:05 -0400

    somewhat broken delete; merging to pull

--- server/cart.js
+++ server/cart.js
@@ -31,4 +31,32 @@ cart.post('/:userId/:productId', (req, res, next) => {
   .catch(next)
 })
 
+cart.delete('/:userId/:productId', (req, res, next) => {
+  console.log(req.user)
+  Cart.findOne({
+    where: {
+      user_id: req.params.userId
+    }
+  })
+  .then(cart => {
+    return CartProduct.findOne({
+      where: {
+        cart_id: cart.id,
+        product_id: req.params.productId
+      }
+    })
+  })
+  .then(item => {
+    console.log('MMMMMMMMMMMMMM', item)
+    const quant = item.quantity
+    console.log('ZZZZZZZZZZZZZZZ', quant)
+    if (quant <= 1) {
+      return item.destroy({ force: true })
+    } else if (quant > 1) {
+      return item.update({quantity: (quant - 1)})
+    }
+  })
+  .catch(next)
+})
+
 module.exports = cart
