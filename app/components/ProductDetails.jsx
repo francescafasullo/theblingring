c9aefa688fade99d7f2f8ba04dbc1e5cbbdc9389
Betty Lin <bettyhappylin@gmail.com>
2017-06-12 19:18:50 -0400

    added bootstrap carousel for product images

--- app/components/ProductDetails.jsx
+++ app/components/ProductDetails.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import {Carousel} from 'react-bootstrap'
 
 export default (props) => {
   const { photos, title, description, price, id } = props.selectedProduct
@@ -7,7 +8,17 @@ export default (props) => {
 
   return (
     <div>
-      <img src={photos ? photos[0] : null}/>
+      <div className="image-gallery">
+        <Carousel>
+          {
+            photos ? photos.map(photoUrl => {
+              return (<Carousel.Item><img src={photoUrl}/></Carousel.Item>)
+            }) : null
+          }
+        </Carousel>
+      </div>
+
+      {/* <img src={photos ? photos[0] : null}/> */}
       <h1>{title}</h1>
       <p>{description}</p>
       <p>${price}</p>
--- app/containers/ProductDetailsContainer.jsx
+++ app/containers/ProductDetailsContainer.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+
 import ProductDetails from '../components/ProductDetails'
 import { addItemToCart } from '../reducers/cart'
 
