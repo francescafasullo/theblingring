2af35d30a5ce5c6dccfc8ca4de0144c48a3f7923
Rachel Berkowitz <rachel.berkowitz@gmail.com>
2017-06-14 10:29:10 -0400

    Create cart.jsx

--- app/reducers/cart.jsx
+++ app/reducers/cart.jsx
@@ -2,13 +2,11 @@ import axios from 'axios'
 
 const initialState = {
   selectedProduct: {},
-  selectedUser: {},
-  totalItems: 0
+  selectedUser: {}
 }
 
 /* ---- actions ---- */
 const ADD_TO_CART = 'ADD_TO_CART'
-const SUM_CART_ITEMS = 'SUM_CART_ITEMS'
 
 /* ---- action creators ---- */
 export const addToCart = (selectedUser, selectedProduct) => ({
@@ -17,32 +15,11 @@ export const addToCart = (selectedUser, selectedProduct) => ({
   selectedProduct
 })
 
-export const sumCartItems = (totalItems) => {
-  console.log('totalitemssssss', totalItems)
-  return {
-    type: SUM_CART_ITEMS,
-    totalItems
-  }
-}
-
 /* ---- dispatchers ---- */
 export const addItemToCart = (user, product) => dispatch => {
   dispatch(addToCart(user.id, product.id))
   axios.post(`/api/cart/${user.id}/${product.id}`)
     .catch(err => console.error(`Adding product: ${product.id} to cart unsuccessful`, err))
-}
-
-export const totalCartItems = (userId) => dispatch => {
-  let totalQuant = 0
-  return axios.get(`/api/cart/${userId}`)
-    .then(cart => {
-      console.log('cart.data', cart.data)
-      for (var i = 0; i < cart.data.cart_products.length; i++) {
-        totalQuant += cart.data.cart_products[i].quantity
-      }
-      dispatch(sumCartItems(totalQuant))
-    })
-    .catch(err => console.error(`Totaling cart items unsuccessful :(`, err))
 }
 
 /* ---- reducer ---- */
@@ -54,10 +31,6 @@ const reducer = (state = initialState, action) => {
   case ADD_TO_CART:
     newState.selectedProduct = action.selectedProduct
     newState.selectedUser = action.selectedUser
-    break
-
-  case SUM_CART_ITEMS:
-    newState.totalItems = action.totalItems
     break
 
   default:
