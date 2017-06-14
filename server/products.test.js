6241a177b180b5ecac1a686eac198be6b3133b3e
Betty Lin <bettyhappylin@gmail.com>
2017-06-14 09:23:51 -0400

    start test writing for products model

--- server/products.test.js
+++ server/products.test.js
@@ -3,9 +3,9 @@ const request = require('supertest'),
   db = require('APP/db'),
   app = require('./start')
 
-// describe undefined? will need to debug
-// describe('/api/products', () => {
-//   before('Await database sync', () => db.didSync)
-//   afterEach('Clear the tables', () => db.truncate({cascade: true }))
+/* global describe it before afterEach */
 
-// })
+describe('/api/products', () => {
+  before('Await database sync', () => db.didSync)
+  afterEach('Clear the tables', () => db.truncate({ cascade: true }))
+})
